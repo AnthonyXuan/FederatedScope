5c270c5badbd4feb2118a736383bedb729f7c0c1
xuan-tidal <anthony2xuan@gmail.com>
2023-08-10 17:31:19 +0800

    update help_scripts & trigger

---
+++ helpful_scripts/extract-poi.py
@@ -0,0 +1,20 @@
+# 这个脚本从输出的log中提取test poison acc，并求平均值。
+import re
+
+# 文件名
+filename = "./POI-LOG.txt"
+
+# 读取文件内容
+with open(filename, "r") as file:
+    content = file.readlines()
+
+# 提取test poisoning accuracy
+test_poisoning_accuracy = [float(re.search("the test poisoning accuracy: (.*)", line).group(1))
+                        for line in content if "the test poisoning accuracy" in line]
+
+# 计算平均值
+average = sum(test_poisoning_accuracy) / len(test_poisoning_accuracy)
+
+print(f"Test Poisoning Accuracy Vector: {test_poisoning_accuracy}")
+print(f"Average Test Poisoning Accuracy: {average}")
+print(f"len:{len(test_poisoning_accuracy)}")
---
+++ helpful_scripts/plot_log.py
@@ -0,0 +1,23 @@
+import json
+import matplotlib.pyplot as plt
+
+# 提供了日志文件的路径
+logfile = '../eval_results.log'
+
+# 提供了你想要绘制的变量的路径
+variable_path = ['Results_weighted_avg', 'test_acc']
+
+# 读取日志文件
+with open(logfile, 'r') as f:
+    logs = [json.loads(line) for line in f]
+
+# 提取训练轮数和你选择的变量
+rounds = [log['Round'] for log in logs]
+values = [log[variable_path[0]][variable_path[1]] for log in logs]
+
+# 绘制图形
+plt.plot(rounds, values)
+plt.xlabel('Round')
+plt.ylabel('.'.join(variable_path))
+plt.savefig('nana.png')
+plt.show()
